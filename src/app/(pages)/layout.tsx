d010c66a8b6740b351453142c75d31190d89a6d9
Joe Maddalone <joe.maddalone@gmail.com>
2025-12-07 12:22:33 -0600

    feat(i18n): add i18n library and tooling
    Initial pass at a workflow for introducing i18n

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Providers } from "@/components/context/Providers";
 import { Navigation } from "@/components/Navigation";
+import { getMessages, getLocale } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
 import "@/styles/globals.css";
 import { api } from "@/lib/api";
 
@@ -24,14 +26,19 @@ export default async function RootLayout({
     console.error(servers.error);
   }
 
+  const messages = await getMessages();
+  const locale = await getLocale();
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body>
-        <Providers>
-          <Navigation session={session?.data} servers={servers?.data} />
-          <main className="container mx-auto px-4 py-8">{children}</main>
-        </Providers>
+        <NextIntlClientProvider messages={messages} locale={locale}>
+          <Providers>
+            <Navigation session={session?.data} servers={servers?.data} />
+            <main className="container mx-auto px-4 py-8">{children}</main>
+          </Providers>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
