4c1308dd6d31b9cdb4dadb59f121aca46817a5cf
Joe Maddalone <joe.maddalone@gmail.com>
2025-12-21 14:41:57 -0600

    refactor: add google fonts to next/csp

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.theposterdb.com",
         pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "fonts.googleapis.com",
+        pathname: "/**",
       }
     ],
   },
